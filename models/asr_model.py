bd0ee0aa06fc09c697b9e4dd25e76808babc69e9
Vlad_K <kalinin.vd@yandex.ru>
2025-02-03 19:12:53 +0300

    Added the main abstract class for all ASR models

---
+++ models/asr_model.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from torch import Tensor, device
+from numpy import ndarray
+
+
+class ASRModel(ABC):
+    name: str
+
+    @abstractmethod
+    def transcribe_file(self, file_path: str) -> str:
+        raise NotImplementedError("transcribe_file method called from abstract class!")
+
+    @abstractmethod
+    def transcribe_wav(self, wav: Tensor | ndarray | list | tuple, sample_rate: int) -> str:
+        raise NotImplementedError("transcribe_wav method called from abstract class!")
+
+    @abstractmethod
+    def move_to_device(self, device: str | device):
+        raise NotImplementedError("move_to_device method called from abstract class!")
+    
+    @abstractmethod
+    def get_current_device(self) -> str:
+        raise NotImplementedError("get_current_device method called from abstract class!")
